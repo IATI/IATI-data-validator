38dabc8ec4da45aff1e94ae18bd043cc01d714e2
Alex Lydiate <alexl@devinit.org>
2020-07-22 16:42:04 +0100

    Added MD5 hashing and used that as file name

--- process.py
+++ process.py
@@ -1,14 +1,19 @@
 import time
 import requests
 import os
+import json
+import hashlib
 
 api = "http://localhost:3000"
 
 while True:
+    try:
+        r = requests.get(api + '/internal/file/next')
+    except:
+        time.sleep(10)
+        continue
 
-    r = requests.get(api + '/internal/file/next')
-
-    if r.status != 200:
+    if r.status_code != 200:
         time.sleep(10)
         continue
 
@@ -21,13 +26,20 @@ while True:
         requests.post(api + '/internal/file/report', payload)
         continue
 
-    os.system('ant -f build-engine.xml -Dfilemask=' + iatiFileData._id + ' feedback')
-    os.system('ant -f build-engine.xml -Dfilemask=' + iatiFileData._id + ' json')
+    iatiFileData['fileMD5'] = hashlib.md5(r.content.encode('utf-8')).hexdigest()
+ 
+    with open('/work/space/input/' + iatiFileData.fileMD5 +'.xml, 'wb') as f:
+        f.write(r.content)
+
+    os.system('ant -f build-engine.xml -Dfilemask=' + iatiFileData.fileMD5 + ' feedback')
+    os.system('ant -f build-engine.xml -Dfilemask=' + iatiFileData.fileMD5 + ' json')
 
     #All the magical knowledge of the paths of the files produced by the commands above is a hangover from the D4D v1
 
-    with open('/work/space/json/' + iatiFileData._id + '.json', 'rb') as f:
-         r = requests.post(api + '/internal/file/report', files={iatiFileData._id + 'json': f})
+    with open('/work/space/json/' + iatiFileData.fileMD5 + '.json', 'rb') as f:
+        iatiFileData['validationReport'] = json.load(f)
     
-    os.remove('/work/space/dest/$ID.feedback.xml')
-    os.remove('/work/space/json/$ID.json')
+    r = requests.post(api + '/internal/file/report', iatiFileData)
+    
+    os.remove('/work/space/dest/' + iatiFileData.fileMD5 + '.feedback.xml')
+    os.remove('/work/space/json/' + iatiFileData.fileMD5 + '.json')
